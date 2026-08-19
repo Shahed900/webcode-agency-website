a3103846c7d96062f25deb216456841527515845
Shahed900 <shahedbin90@gmail.com>
2026-08-20 04:17:29 +0600

    Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [pageLoading, setPageLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setPageLoading(false);
+    };
+
+    if (document.readyState === "complete") {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setPageLoading(false);
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
+
+  if (pageLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
   return (
     <div>
       <Navbar />
